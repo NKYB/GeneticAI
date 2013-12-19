18ea850c46710776a68f20ded9fee0fbc7d67ca7
NKYB <3k2bar@gmail.com>
2013-12-18 21:06:53 -0500

    Tested with aparapi jar

--- src/main/java/com/propaganda905/geneticai/App.java
+++ src/main/java/com/propaganda905/geneticai/App.java
@@ -1,9 +1,24 @@
 package com.propaganda905.geneticai;
 
+import com.amd.aparapi.Kernel;
+import com.amd.aparapi.Range;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        final float[] result = new float[20];
+
+        Kernel kernel = new Kernel(){
+            @Override public void run(){
+               int i= getGlobalId();
+               result[i]=5;
+            }
+        };
+        Range range = Range.create(result.length); 
+        kernel.execute(range);
+        System.out.println( "Result[0]: " + result[0] );
+        kernel.dispose();
     }
 }
